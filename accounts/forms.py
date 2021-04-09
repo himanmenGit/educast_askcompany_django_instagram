0a436b2f5e3d2d0b11f592753eac14dfc56001c0
himanmenGit <study.himanmen@gmail.com>
2021-04-09 09:55:45 +0900

    에듀캐스트 리액트와 함께 장고 시작하기 챕터7 (10-auth앱의 PasswordChageForm 커스텀을 통함 안호 수정 구현)

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    PasswordChangeForm as AuthPasswordChangeForm
+)
 
 from .models import User
 
@@ -36,3 +39,14 @@ class ProfileForm(forms.ModelForm):
             'phone_number',
             'gender'
         ]
+
+
+class PasswordChangeForm(AuthPasswordChangeForm):
+    def clean_new_password1(self):
+        old_password = self.cleaned_data.get("old_password")
+        new_password1 = self.cleaned_data.get("new_password1")
+
+        if old_password and new_password1:
+            if old_password == new_password1:
+                raise forms.ValidationError("새로운 암호는 기존암호와 다르게 입력 해 주세요.")
+        return new_password1
